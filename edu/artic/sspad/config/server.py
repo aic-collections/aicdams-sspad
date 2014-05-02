2503640ba54ffd6200206f2cdf8a82d0a6408399
Stefano Cossu <scossu@artic.edu>
2014-05-02 10:08:04 -0500

    Added basic logging.

--- edu/artic/sspad/config/server.py
+++ edu/artic/sspad/config/server.py
@@ -4,6 +4,8 @@ conf = {
     'global': {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': 5000,
+		'log.access_file': '/var/log/sspad/access.log',
+		'log.error_file': '/var/log/sspad/error.log',
     },
     '/': {
         'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
