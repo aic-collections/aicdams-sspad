c247154cbad540a2ac60555e2310e55e41b75622
Stefano Cossu <scossu@artic.edu>
2014-07-09 16:25:34 -0500

    Moved config update; added daemonizer.

--- sspad.py
+++ sspad.py
@@ -1,16 +1,18 @@
 import cherrypy
 
+from cherrypy.process.plugins import Daemonizer
+
 from edu.artic.sspad.config import server, app
 from edu.artic.sspad.modules import resource, staticImage
 
 class Webapp():
-	cherrypy.config.update(server.conf)
 	exposed = True
 
 	resource = resource.Resource()
 	resource.SI = staticImage.StaticImage()
 	#@TODO Add other resource prefixes.
-	cherrypy.log('Cherrypy config: ' + str(cherrypy.config))
+
+	#cherrypy.log('Cherrypy config: ' + str(cherrypy.config))
 
 	def GET(self):
 		return {'message': 'Nothing to see here.'}
@@ -18,6 +20,11 @@ class Webapp():
 
 #print('Name:', __name__)
 if __name__ == 'sspad' or __name__ == '__main__':
-	cherrypy.tree.mount(Webapp(), '/', app.rest_conf)
+	cherrypy.config.update(server.conf)
+
+	Daemonizer(cherrypy.engine).subscribe()
+
+	webapp = Webapp()
+	cherrypy.tree.mount(webapp, '/', app.rest_conf)
 	cherrypy.engine.start()
 	cherrypy.engine.block()
