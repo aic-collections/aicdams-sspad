78b955ea3762fe9823887bf325b95ed6f77aaa9f
Stefano Cossu <scossu@artic.edu>
2014-08-06 16:57:33 -0500

    Re-added sspad.config.datasources.

---
+++ sspad/config/datasources.py
@@ -0,0 +1,49 @@
+## Config file for all data source settings.
+#
+#  This file is under source control. DO NOT PUT ANY HOST-SPECIFIC AND LESS THAN EVER SENSITIVE DATA HERE!
+#  All host-dependent settings should be imported from host.conf.
+#  If some data source is consistent across environments, settings such as hostname and path can be hard-coded here.
+
+from sspad.config import host
+
+uidminter_db = host.uidminter_db
+uidminter_db['conn_string'] = 'host={} port={} user={} password={} dbname={}'.format(
+	uidminter_db['host'],
+	uidminter_db['port'],
+	uidminter_db['username'],
+	uidminter_db['password'],
+	uidminter_db['db']
+)
+
+
+datagrinder_rest_api = host.datagrinder_rest_api
+datagrinder_rest_api['base_url'] = '{}://{}{}'.format(
+	datagrinder_rest_api['proto'],
+	datagrinder_rest_api['host'],
+	datagrinder_rest_api['root']
+)
+
+
+lake_rest_api = host.lake_rest_api
+lake_rest_api['base_url'] = '{}://{}{}'.format(
+	lake_rest_api['proto'],
+	lake_rest_api['host'],
+	lake_rest_api['root']
+)
+
+
+tstore_rest_api = host.tstore_rest_api
+tstore_rest_api['base_url'] = '{}://{}{}'.format(
+	tstore_rest_api['proto'],
+	tstore_rest_api['host'],
+	tstore_rest_api['root']
+)
+
+
+tstore_schema_rest_api = host.tstore_schema_rest_api
+tstore_schema_rest_api['base_url'] = '{}://{}{}'.format(
+	tstore_schema_rest_api['proto'],
+	tstore_schema_rest_api['host'],
+	tstore_schema_rest_api['root']
+)
+
