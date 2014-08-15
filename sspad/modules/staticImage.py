a1a810a4698ca074c7038a8836d8ff5d9c4d8991
Stefano Cossu <scossu@artic.edu>
2014-08-15 12:22:26 -0500

    Removed residual references to ns_aic.* #2.

--- sspad/modules/staticImage.py
+++ sspad/modules/staticImage.py
@@ -128,8 +128,8 @@ class StaticImage(Resource):
 			prop_tuples = [
 				(ns_collection['rdf'].type, ns_collection['aic'].image),
 				(ns_collection['rdf'].type, ns_collection['aic'].citi),
-				(ns_collection['dc.title'], Literal(uid)),
-				(ns_collection['aic.uid'], Literal(uid)),
+				(ns_collection['dc'].title, Literal(uid)),
+				(ns_collection['aic'].uid, Literal(uid)),
 			]
 
 			for req_name, lake_name in zip(self.prop_req_names, self.prop_lake_names):
@@ -165,7 +165,7 @@ class StaticImage(Resource):
 				# Set source datastream properties
 				prop_tuples = [
 							(ns_collection['rdf'].type, ns_collection['indexing'].indexable),
-							(ns_collection['dc.title'], Literal(uid + '_' + dsname)),
+							(ns_collection['dc'].title, Literal(uid + '_' + dsname)),
 						]
 				if dsname == 'master':
 					prop_tuples.append((ns_collection['rdf'].type, ns_collection['aicmix'].imageDerivable))
