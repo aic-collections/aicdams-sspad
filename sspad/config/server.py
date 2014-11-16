2c04ff68e7ae023253d2cd7b3fa67ca707aecc6e
Stefano Cossu <scossu@artic.edu>
2014-11-15 22:11:02 -0600

    Split up logfile directories.

--- sspad/config/server.py
+++ sspad/config/server.py
@@ -2,11 +2,20 @@ import cherrypy
 
 from sspad.config import host
 
+if host.app_env == 'prod':
+    logdir = '/var/log/sspad/'
+elif host.app_env == 'stag':
+    logdir = '/var/log/sspad-staging/'
+elif host.app_env == 'test':
+    logdir = '/var/log/sspad-test/'
+else:
+    logdir = '/var/log/sspad-dev/'
+
 ## CherryPy server configuration.
 conf = {
     'global': {
-		'log.access_file': '/var/log/sspad/access.log',
-		'log.error_file': '/var/log/sspad/error.log',
+		'log.access_file': logdir + 'access.log',
+		'log.error_file': logdir + 'error.log',
 		'server.max_request_body_size': host.max_req_size,
 		'server.socket_host': host.listen_addr,
 		'server.socket_port': host.listen_port,
