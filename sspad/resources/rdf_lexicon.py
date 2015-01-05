868aab1e4232f2b04fec17bd5a1d8fd187f1c35f
Stefano Cossu <scossu@artic.edu>
2015-01-05 14:12:22 -0600

    Add ore and edm definitions and change URI for aic: prefix.

--- sspad/resources/rdf_lexicon.py
+++ sspad/resources/rdf_lexicon.py
@@ -3,14 +3,15 @@ from rdflib import Graph
 from rdflib.namespace import Namespace, NamespaceManager
 
 ns_collection = {
-    'aic':              Namespace('http://artic.edu/definitions/ontology/1.0#'),
-    'aicdb':            Namespace('http://artic.edu/definitions/ontology/1.0/dbconn#'),
-    'aiclist':          Namespace('http://artic.edu/definitions/ontology/1.0/auth_list#'),
-    'aicmeta':          Namespace('http://artic.edu/definitions/ontology/1.0/metadata#'),
-    'aicmix':           Namespace('http://artic.edu/definitions/ontology/1.0/mixin#'),
+    'aic':              Namespace('http://definitions.artic.edu/ontology#'),
+    'aicdb':            Namespace('http://definitions.artic.edu/ontology/dbconn#'),
+    'aiclist':          Namespace('http://definitions.artic.edu/ontology/auth_list#'),
+    'aicmeta':          Namespace('http://definitions.artic.edu/ontology/metadata#'),
+    'aicmix':           Namespace('http://definitions.artic.edu/ontology/mixin#'),
     'authz':            Namespace('http://fedora.info/definitions/v4/authorization#'),
     'cidoc':            Namespace('http://www.cidoc-crm.org/cidoc-crm/'),
     'dc':               rdflib.namespace.DC,
+    'edm':              Namespace('http:/www.europeana.eu/schemas/edm/'),
     'exif':             Namespace('http://www.w3.org/2003/12/exif/ns#'),
     'fcrepo':           Namespace('http://fedora.info/definitions/v4/repository#'),
     'fedora':           Namespace('http://fedora.info/definitions/v4/rest-api#'),
@@ -24,6 +25,7 @@ ns_collection = {
     'mode':             Namespace('http://www.modeshape.org/1.0'),
     'nt':               Namespace('http://www.jcp.org/jcr/nt/1.0'),
     'premis':           Namespace('http://www.loc.gov/premis/rdf/v1#'),
+    'ore':              Namespace('http://www.openarchives.org/ore/terms/'),
     'rdf':              rdflib.namespace.RDF,
     'rdfs':             rdflib.namespace.RDFS,
     'skos':             rdflib.namespace.SKOS,
